9804841903067363b3d7312701d2b41f8f096034
迷渡 <justjavac@gmail.com>
2022-08-04 08:12:14 +0800

    upgrade std@0.150 (#11)

--- mod_test.ts
+++ mod_test.ts
@@ -1,7 +1,7 @@
 import {
   assertEquals,
   assertThrows,
-} from "https://deno.land/std@0.68.0/testing/asserts.ts";
+} from "https://deno.land/std@0.150.0/testing/asserts.ts";
 
 import dijkstra from "./mod.ts";
 
